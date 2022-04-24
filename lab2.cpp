b8354ccd0d90451bd7d72dc47efd4b3adf5f402c
ArekK <arkadiusz.koszewski@frey.pl>
2022-04-24 18:07:15 +0200

    Wszystkie funkcje w klasie bazy

--- lab2.cpp
+++ lab2.cpp
@@ -7,45 +7,6 @@
 
 using namespace std;
 
-#define CHECK_ERROR(code, str, h, env)  \
-    if (code != SQL_SUCCESS)            \
-    {                                   \
-        extract_error(str, h, env);     \
-        clearHandle(hstmt, hdbc, henv); \
-        exit(0);                        \
-    }
-
-void extract_error(string fn, SQLHANDLE handle, SQLSMALLINT type)
-{
-    SQLINTEGER i = 0;
-    SQLINTEGER native;
-    SQLCHAR state[7];
-    SQLCHAR text[256];
-    SQLSMALLINT len;
-    SQLRETURN ret;
-    printf("\nThe driver reported the following diagnostics whilst running %s\n\n", fn.c_str());
-    do
-    {
-        ret = SQLGetDiagRec(type, handle, ++i, state, &native, text, sizeof(text), &len);
-        printf("\nSQLGetDiagRec returned %d\n\r", ret);
-        if (SQL_SUCCEEDED(ret))
-            printf("%s:%ld:%ld:%s\n", state, (long)i, (long)native, text);
-    } while (ret == SQL_SUCCESS);
-}
-
-void clearHandle(SQLHSTMT hstmt, SQLHDBC hdbc, SQLHENV henv)
-{
-    if (hstmt != SQL_NULL_HSTMT)
-        SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
-    if (hdbc != SQL_NULL_HDBC)
-    {
-        SQLDisconnect(hdbc);
-        SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
-    }
-    if (henv != SQL_NULL_HENV)
-        SQLFreeHandle(SQL_HANDLE_ENV, henv);
-}
-
 class Patient
 {
 public:
@@ -74,7 +35,7 @@ void* operator new(size_t size)
 
 void operator delete(void* ptr)
 {
-    cout << "Dealokacja" << endl;
+    cout << "Dealokacja: " << endl;
     free(ptr);
 }
 
@@ -87,21 +48,21 @@ public:
       : henv(SQL_NULL_HENV), hdbc(SQL_NULL_HDBC), hstmt(SQL_NULL_HSTMT)
     {
         retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);
-        CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_ENV)", henv, SQL_HANDLE_ENV);
+        checkError(retcode, "SQLAllocHandle(SQL_HANDLE_ENV)", henv, SQL_HANDLE_ENV);
         retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER *)SQL_OV_ODBC3, 0);
-        CHECK_ERROR(retcode, "SQLSetEnvAttr(SQL_ATTR_ODBC_VERSION)", henv, SQL_HANDLE_ENV);
+        checkError(retcode, "SQLSetEnvAttr(SQL_ATTR_ODBC_VERSION)", henv, SQL_HANDLE_ENV);
         retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc);
-        CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_DBC)", hdbc, SQL_HANDLE_DBC);
+        checkError(retcode, "SQLAllocHandle(SQL_HANDLE_DBC)", hdbc, SQL_HANDLE_DBC);
         retcode = SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0);
-        CHECK_ERROR(retcode, "SQLSetConnectAttr(SQL_LOGIN_TIMEOUT)", hdbc, SQL_HANDLE_DBC);
+        checkError(retcode, "SQLSetConnectAttr(SQL_LOGIN_TIMEOUT)", hdbc, SQL_HANDLE_DBC);
 
         retcode = SQLConnect(hdbc, (SQLCHAR *)dbname.c_str(), SQL_NTS, (SQLCHAR *)NULL, SQL_NTS, NULL, SQL_NTS);
-        CHECK_ERROR(retcode, "SQLConnect(DATASOURCE)", hdbc, SQL_HANDLE_DBC);
+        checkError(retcode, "SQLConnect(DATASOURCE)", hdbc, SQL_HANDLE_DBC);
     }
 
     ~DbManager() 
     {
-        clearHandle(hstmt, hdbc, henv);
+        freeResources();
     }
 
     void selectFromPatients()
@@ -110,23 +71,23 @@ public:
         Patient patient;
 
         retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
-        CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_STMT)", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLAllocHandle(SQL_HANDLE_STMT)", hstmt, SQL_HANDLE_STMT);
 
         string command = "select * from patients";
         retcode = SQLExecDirect(hstmt, (SQLCHAR *)command.c_str(), SQL_NTS);
-        CHECK_ERROR(retcode, "SQLExecDirect() SELECT", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLExecDirect() SELECT", hstmt, SQL_HANDLE_STMT);
 
         retcode = SQLBindCol(hstmt, 1, SQL_C_USHORT, &patient.id, 2, &len);
-        CHECK_ERROR(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
 
         retcode = SQLBindCol(hstmt, 2, SQL_C_CHAR, &patient.name, 20, &len);
-        CHECK_ERROR(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
 
         retcode = SQLBindCol(hstmt, 3, SQL_C_CHAR, &patient.surname, 20, &len);
-        CHECK_ERROR(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
 
         retcode = SQLBindCol(hstmt, 4, SQL_C_CHAR, &patient.pesel, 11, &len);
-        CHECK_ERROR(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
 
         retcode = SQLFetch(hstmt);
         while (retcode != SQL_NO_DATA)
@@ -143,23 +104,23 @@ public:
         Study study;
 
         retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
-        CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_STMT)", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLAllocHandle(SQL_HANDLE_STMT)", hstmt, SQL_HANDLE_STMT);
 
         string command = "select * from studies";
         retcode = SQLExecDirect(hstmt, (SQLCHAR *)command.c_str(), SQL_NTS);
-        CHECK_ERROR(retcode, "SQLExecDirect() SELECT", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLExecDirect() SELECT", hstmt, SQL_HANDLE_STMT);
 
         retcode = SQLBindCol(hstmt, 1, SQL_C_USHORT, &study.patient_id, 2, &len);
-        CHECK_ERROR(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
 
         retcode = SQLBindCol(hstmt, 2, SQL_C_CHAR, &study.type, 20, &len);
-        CHECK_ERROR(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
 
         retcode = SQLBindCol(hstmt, 3, SQL_C_CHAR, &study.date, 20, &len);
-        CHECK_ERROR(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
 
         retcode = SQLBindCol(hstmt, 4, SQL_C_CHAR, &study.result, 20, &len);
-        CHECK_ERROR(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLBindCol()", hstmt, SQL_HANDLE_STMT);
 
         retcode = SQLFetch(hstmt);
         while (retcode != SQL_NO_DATA)
@@ -173,24 +134,24 @@ public:
     void insertPatient(Patient patient)
     {
         retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
-        CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_STMT)", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLAllocHandle(SQL_HANDLE_STMT)", hstmt, SQL_HANDLE_STMT);
 
         string komenda = "INSERT INTO Patients (name, surname, pesel) VALUES ('"+string((char*)patient.name)+ "','" +string((char*)patient.surname)+"','"+string((char*)patient.pesel)+"');";
         cout << "Komenda: " << komenda << endl;
         retcode = SQLExecDirect(hstmt, (SQLCHAR *)komenda.c_str(), SQL_NTS);
-        CHECK_ERROR(retcode, "SQLExecDirect() INSERT", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLExecDirect() INSERT", hstmt, SQL_HANDLE_STMT);
 
         retcode = SQLFreeStmt(hstmt, SQL_DROP);
     }
     void insertStudy(Study study)
     {
         retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
-        CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_STMT)", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLAllocHandle(SQL_HANDLE_STMT)", hstmt, SQL_HANDLE_STMT);
 
         string komenda = "INSERT INTO Studies (patient_id, type, date, result) VALUES ('"+to_string(study.patient_id)+"','"+string((char*)study.type)+"','"+string((char*)study.date)+"','"+string((char*)study.result)+"');";
         cout << "Komenda: " << komenda << endl;
         retcode = SQLExecDirect(hstmt, (SQLCHAR *)komenda.c_str(), SQL_NTS);
-        CHECK_ERROR(retcode, "SQLExecDirect() INSERT", hstmt, SQL_HANDLE_STMT);
+        checkError(retcode, "SQLExecDirect() INSERT", hstmt, SQL_HANDLE_STMT);
 
         retcode = SQLFreeStmt(hstmt, SQL_DROP);
     }
@@ -199,6 +160,47 @@ private:
     SQLHDBC hdbc;
     SQLHSTMT hstmt;
     SQLRETURN retcode;
+
+    void checkError(SQLRETURN code, const char* str, SQLHANDLE h, SQLSMALLINT env)
+    {
+        if (code != SQL_SUCCESS)
+        {
+            extract_error(str, h, env);
+            freeResources();
+            exit(0); 
+        }
+    }
+    void freeResources()
+    {
+        if (hstmt != SQL_NULL_HSTMT) {
+            SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+        }
+        if (hdbc != SQL_NULL_HDBC)
+        {
+            SQLDisconnect(hdbc);
+            SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+        }
+        if (henv != SQL_NULL_HENV) {
+            SQLFreeHandle(SQL_HANDLE_ENV, henv);
+        }
+    }
+    void extract_error(string fn, SQLHANDLE handle, SQLSMALLINT type)
+    {
+        SQLINTEGER i = 0;
+        SQLINTEGER native;
+        SQLCHAR state[7];
+        SQLCHAR text[256];
+        SQLSMALLINT len;
+        SQLRETURN ret;
+        printf("\nThe driver reported the following diagnostics whilst running %s\n\n", fn.c_str());
+        do
+        {
+            ret = SQLGetDiagRec(type, handle, ++i, state, &native, text, sizeof(text), &len);
+            printf("\nSQLGetDiagRec returned %d\n\r", ret);
+            if (SQL_SUCCEEDED(ret))
+                printf("%s:%ld:%ld:%s\n", state, (long)i, (long)native, text);
+        } while (ret == SQL_SUCCESS);
+    }
 };
 
 Patient createPatient()
